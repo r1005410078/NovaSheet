96139e78a8bd7f96cb878eb14b6be51bac4c0c48
rongtaosheng <rongtaosheng@econ-tech.cn>
2026-06-13 04:57:57 +0800

    fix(canvas2d): 导出单元格 action zone 类型

--- packages/canvas2d/src/index.ts
+++ packages/canvas2d/src/index.ts
@@ -11,6 +11,7 @@ export type { Canvas2DRendererOptions } from './render/Canvas2DRenderer'
 export { CANVAS2D_PAINT_LAYERS } from './render/PaintLayer'
 export type { Canvas2DPaintLayer } from './render/PaintLayer'
 export type {
+  Canvas2DCellActionZone,
   Canvas2DCellRenderParams,
   Canvas2DCellRenderer,
   Canvas2DCellRendererRegistry,
